5c69aaa83d2b5d74f6e4e8f9af9817fc638f0182
Bakhtin <jurij.bakhtin@contractor.calleridreputation.com>
2022-09-30 17:21:51 +0700

    Fatures: - implemented trigger slave lambda functions - implemented aws lambda packege for GO

--- main.go
+++ main.go
@@ -1,11 +1,75 @@
 package main
 
-func main() {
-	go test()
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+const DB_HOST = "cid-rep-dev-v2.cluster-cxkxcvxokdny.us-east-2.rds.amazonaws.com"
+const DB_PORT = "3306"
+const DB_USER = "calleridrepdev"
+const DB_NAME = "calleridrep_db"
+const DB_PASSWORD = "STAZP!^KUEx4jE4jpmUC2Exbp6"
+
+var database *sql.DB
+
+type MyEvent struct {
+	Name string `json:"What is your name?"`
+	Age int     `json:"How old are you?"`
 }
 
-func test(){
-	//time.Sleep(50)
+type MyResponse struct {
+	Message string `json:"Answer:"`
+}
 
-	println("Hello")
+type ServerRegion struct{
+	Id int
+	Host string
+}
+
+func request(url string) {
+	_, err := http.Get(url)
+
+	if err == nil {
+		log.Println(err)
+	}
+}
+
+func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
+	rows, err := database.Query("select id, host from stir_shaken_region_servers where enabled = 1")
+	if err != nil {
+		log.Println(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		p := ServerRegion{}
+		err := rows.Scan(&p.Id, &p.Host)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		go request(p.Host)
+	}
+
+	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+}
+
+func main() {
+	db, err := sql.Open("mysql", DB_USER + ":" + DB_PASSWORD + "@tcp(" + DB_HOST + ":" + DB_PORT + ")/" + DB_NAME )
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	database = db
+
+	defer db.Close()
+
+	lambda.Start(HandleLambdaEvent)
 }
